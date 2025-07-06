dd3450a368548d73b68ef5df3b652c4469414511
Pratyakshgupta887qwert <pratyakshgupta887@gmail.com>
2025-07-07 01:06:42 +0530

    Assignment Number: 54.1from branch : AssignmentNo54.1

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <nav className="bg-white shadow-md">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRAMo2vwItqA-C-Xkn5e645lr8MIKOt3ON8-w&s" />
+        </nav>
+    </div>
+  )
+}
