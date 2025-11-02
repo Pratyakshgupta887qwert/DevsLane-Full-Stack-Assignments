27672e1a2743535c96ca8cfc5e3776c75f216c00
Pratyakshgupta887qwert <pratyakshgupta887@gmail.com>
2025-11-02 21:21:36 +0530

    assignment No 59.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import React, { useState, useCallback, useMemo } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import ItemListPage from './components/ItemsListPage';
+import Details from './components/Details';
+import CartPage from './components/CartPage';
+import NotFound from './components/NotFound';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const path = window.location.pathname;
+  const savedCartString = localStorage.getItem("myCart") || "{}";
+  const savedData = JSON.parse(savedCartString);
+
+  const [cart, setCart] = useState(savedData);
+
+  const handleAddToCart = useCallback(function (productId, count) {
+    const oldCount = cart[productId] || 0;
+    const newCart = { ...cart, [productId]: oldCount + count };
+    updateCart(newCart);
+  }, [cart]);
+  function updateCart(newCart) {
+    setCart(newCart);
+    const cartString = JSON.stringify(newCart);
+    localStorage.setItem("myCart", cartString);
+  }
+
+  const totalCount = useMemo(function () {
+    return Object.keys(cart).reduce(function (previous, current) {
+      return previous + cart[current];
+    }, 0);
+  }, [cart]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='bg-stone-100 flex flex-col h-screen overflow-auto' >
+      <Navbar totalCount={totalCount} />
+      <div className='grow' >
+        <Routes>
+          <Route index element={<ItemListPage />} />
+          <Route path='/details/:id' element={<Details onAddToCart={handleAddToCart} />} />
+          <Route path='/cart' element={<CartPage cartData={savedData} updateCart={updateCart} />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Footer />
+    </div>
+  );
+
 }
 
 export default App
